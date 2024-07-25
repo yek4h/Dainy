2f3e017ca019c1982320476c68d8244d64f5de3b
yek4h <antonioonwn22@gmail.com>
2024-07-25 13:33:48 -0400

    feat: enhance Scenario class with event handling methods

--- src/main/kotlin/vip/aridi/meetup/scenario/Scenario.kt
+++ src/main/kotlin/vip/aridi/meetup/scenario/Scenario.kt
@@ -1,9 +1,13 @@
 package vip.aridi.meetup.scenario
 
 import org.bukkit.Bukkit
+import org.bukkit.event.Event
+import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.inventory.ItemStack
 import org.example.vip.aridi.meetup.Dainy
+import kotlin.reflect.KClass
 
 /*
  * This project can't be redistributed without
@@ -21,8 +25,50 @@ abstract class Scenario(
 ) : Listener {
 
     var isActive: Boolean = false
+        private set
+
+    private val eventListeners = mutableListOf<Listener>()
 
     fun handleToggle() {
-        Bukkit.getPluginManager().registerEvents(this, Dainy.get())
+        isActive = !isActive
+        if (isActive) {
+            onEnable()
+        } else {
+            onDisable()
+        }
     }
+
+    open fun onEnable() {
+        registerEvents()
+    }
+
+    open fun onDisable() {
+        unregisterEvents()
+    }
+
+    private fun registerEvents() {
+        eventListeners.forEach {
+            Bukkit.getPluginManager().registerEvents(it, Dainy.get())
+        }
+    }
+
+    private fun unregisterEvents() {
+        eventListeners.forEach {
+            HandlerList.unregisterAll(it)
+        }
+    }
+
+    protected fun <T : Event> subscribe(eventClass: KClass<T>, handler: (T) -> Unit) {
+        val listener = object : Listener {
+            @Suppress("UNCHECKED_CAST")
+            @EventHandler
+            fun onEvent(event: Event) {
+                if (eventClass.java.isAssignableFrom(event::class.java)) {
+                    handler(event as T)
+                }
+            }
+        }
+        eventListeners.add(listener)
+    }
+
 }
