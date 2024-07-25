4f5a1fd4982b031efa7c7cb3c9aaf0ed10af26f2
yek4h <antonioonwn22@gmail.com>
2024-07-25 12:44:02 -0400

    feat: first no scenario game

---
+++ src/main/kotlin/vip/aridi/meetup/scenario/sub/NoScenario.kt
@@ -0,0 +1,22 @@
+package vip.aridi.meetup.scenario.sub
+
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import vip.aridi.meetup.scenario.Scenario
+
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Dainy
+ * @author yek4h © 2024
+ * Date: 25/07/2024
+*/
+
+class NoScenario: Scenario(
+    name = "",
+    icon = ItemStack(Material.PAPER),
+    description = "No modifications, just the normal game"
+){
+}
