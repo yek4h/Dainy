a6a7acf5719532ee2c77d8cd641d0f4d96ed9b31
yek4h <antonioonwn22@gmail.com>
2024-07-26 09:54:09 -0400

    feat: Game classes

---
+++ src/main/kotlin/vip/aridi/meetup/game/MeetupGameListener.kt
@@ -0,0 +1,17 @@
+package vip.aridi.meetup.game
+
+import org.bukkit.event.Listener
+
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Dainy
+ * @author yek4h © 2024
+ * Date: 26/07/2024
+*/
+
+class MeetupGameListener(
+): Listener {
+}
---
+++ src/main/kotlin/vip/aridi/meetup/game/MeetupManager.kt
@@ -0,0 +1,20 @@
+package vip.aridi.meetup.game
+
+import vip.aridi.meetup.loader.Loader
+
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Dainy
+ * @author yek4h © 2024
+ * Date: 25/07/2024
+*/
+
+class MeetupManager(
+) {
+
+    init {
+    }
+}
