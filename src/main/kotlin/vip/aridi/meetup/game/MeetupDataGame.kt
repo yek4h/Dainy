4571c1cdc16cadeed815c2e92e157cbfba8b74c0
yek4h <antonioonwn22@gmail.com>
2024-07-27 23:04:40 -0400

    feat: status, border format, and misc things

--- src/main/kotlin/vip/aridi/meetup/game/MeetupDataGame.kt
+++ src/main/kotlin/vip/aridi/meetup/game/MeetupDataGame.kt
@@ -35,4 +35,45 @@ class MeetupDataGame {
     var loadingMessage: String = ""
 
     var currentState: MeetupState = MeetupState.WAITING
+
+    fun getNextBorderSize(): Int {
+        return when (borderSize) {
+            100 -> 75
+            75 -> 50
+            50 -> 25
+            else -> 100
+        }
+    }
+
+    fun getStatus(): String {
+        return when (currentState) {
+            MeetupState.IN_GAME -> MeetupState.IN_GAME.formatted()
+            MeetupState.STARTING_GAME -> MeetupState.STARTING_GAME.formatted()
+            MeetupState.WAITING -> MeetupState.WAITING.formatted()
+            MeetupState.GAME_ENDED -> MeetupState.GAME_ENDED.formatted()
+            else -> "&cSetup"
+        }
+    }
+
+    fun getFormattedBorderStatus(): String {
+        return if (borderDuration > 0) {
+            CC.translate("&7 (" + if (borderDuration <= 60) "$borderDuration s" else "${borderDuration / 60 + 1} m" + ")")
+        } else {
+            ""
+        }
+    }
+
+    fun decreaseBorderTime(): Int {
+        borderDuration -= 1
+        return borderDuration
+    }
+
+    fun getElapsedTime(): Int {
+        return ((System.currentTimeMillis() - startTime) / 1000).toInt()
+    }
+
+    fun decreaseEndTime(): Int {
+        endDuration -= 1
+        return endDuration
+    }
 }
